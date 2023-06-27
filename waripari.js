c3fc72d0b6189dc0fb588301b9bdd51b9b2f0a18
uttsav1025 <74333675+uttsav1025@users.noreply.github.com>
2023-06-27 23:42:57 +0545

    added files#1

---
+++ waripari.js
@@ -0,0 +1,6 @@
+var map = L.map("mapContainer").setView([27.7911, 85.3706], 12);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(map);
